d7ca0ab0598015519a7b4f7b94260a347a0dd5a6
gustacodes <gustavorique@live.com>
2025-09-30 16:10:26 -0300

    feat: implementado configuracao para salvar no bucket

---
+++ faturamento/src/main/java/io/github/gustacodes/icompras/faturamento/service/NotaFiscalService.java
@@ -0,0 +1,47 @@
+package io.github.gustacodes.icompras.faturamento.service;
+
+import io.github.gustacodes.icompras.faturamento.model.Pedido;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class NotaFiscalService {
+
+    @Value("classpath:reports/nota-fiscal.jrxml")
+    private Resource notaFiscal;
+
+    @Value("classpath:reports/logo.png")
+    private Resource logo;
+
+    public byte[] gerarNota(Pedido pedido) {
+        try (InputStream inputStream = notaFiscal.getInputStream()) {
+            Map<String, Object> params = new HashMap<>();
+
+            params.put("NOME", pedido.cliente().nome());
+            params.put("CPF", pedido.cliente().cpf());
+            params.put("BAIRRO", pedido.cliente().bairro());
+            params.put("LOGRADOURO", pedido.cliente().logradouro());
+            params.put("NUMERO", pedido.cliente().numero());
+            params.put("EMAIL", pedido.cliente().email());
+            params.put("TELEFONE", pedido.cliente().telefone());
+            params.put("DATA_PEDIDO", pedido.data());
+            params.put("TOTAL_PEDIDO", pedido.total());
+
+            var dataSource = new JRBeanCollectionDataSource(pedido.itens());
+
+            JasperReport jasperReport = JasperCompileManager.compileReport(inputStream);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, dataSource);
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
--- faturamento/src/main/java/io/github/gustacodes/icompras/faturamento/subscriber/GeradorNotaFiscalService.java
+++ faturamento/src/main/java/io/github/gustacodes/icompras/faturamento/subscriber/GeradorNotaFiscalService.java
@@ -1,14 +1,40 @@
 package io.github.gustacodes.icompras.faturamento.subscriber;
 
+import io.github.gustacodes.icompras.faturamento.bucket.BucketFile;
+import io.github.gustacodes.icompras.faturamento.bucket.BucketService;
 import io.github.gustacodes.icompras.faturamento.model.Pedido;
+import io.github.gustacodes.icompras.faturamento.service.NotaFiscalService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class GeradorNotaFiscalService {
+
+    private final NotaFiscalService notaFiscalService;
+    private final BucketService bucketService;
 
     public void gerar(Pedido pedido) {
         log.info("Gerada nota fiscal para o pedido {} ", pedido.codigo());
+        byte[] byteArray = notaFiscalService.gerarNota(pedido);
+
+        try {
+            String nomeArquivo = String.format("notafiscal_pedido_%d.pdf", pedido.codigo());
+
+            var file = new BucketFile(
+                    nomeArquivo,
+                    new ByteArrayInputStream(byteArray),
+                    MediaType.APPLICATION_PDF,
+                    byteArray.length);
+
+            bucketService.upload(file);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
